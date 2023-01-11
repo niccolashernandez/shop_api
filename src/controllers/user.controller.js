e3e4d2962219c716a2809f59fb6142a9b5df791e
Nicolás Hernández <ericknicolashernandezdiaz@gmail.com>
2023-01-11 16:25:33 -0500

    Prueba 3

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { pool } from '../db.js'
 
 export const getUsers = async (req, res) => {
     try {
-        const [rows] = await pool.query('SELECT * FROM usuario')
+        const [rows] = await pool.query('SELECT * FROM USUARIO')
         res.json(rows)
     } catch (error) {
         return res.status(500).json({
